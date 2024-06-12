f64db795e391e8f27e5e8b3c4275f5b7aa37fb6c
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-06-12 13:07:32 +0100

    Rocket Launcher
    Completed lecture 137

--- Blaster/Source/Blaster/Weapon/Projectile.h
+++ Blaster/Source/Blaster/Weapon/Projectile.h
@@ -25,7 +25,11 @@ protected:
 	UPROPERTY(EditAnywhere)
 	float Damage = 20.f;
 
-private:
+	UPROPERTY(EditAnywhere)
+	class UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
 
 	UPROPERTY(EditAnywhere)
 	class UBoxComponent* CollisionBox;
@@ -33,17 +37,12 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	class UProjectileMovementComponent* ProjectileMovementComponent;
 
+private:
 	UPROPERTY(EditAnywhere)
-	class UParticleSystem* Tracer;
+	UParticleSystem* Tracer;
 
 	UPROPERTY()
 	class UParticleSystemComponent* TracerComponent;
-
-	UPROPERTY(EditAnywhere)
-	UParticleSystem* ImpactParticles;
-
-	UPROPERTY(EditAnywhere)
-	class USoundCue* ImpactSound;
 
 public:	
 	
