d2d1ae24380393e2121c1409d6ece564a235677e
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-06-19 19:31:17 +0100

    Adding grenade assets + reload fix
    lecture 153 completed

--- Blaster/Source/Blaster/Character/BlasterCharacter.h
+++ Blaster/Source/Blaster/Character/BlasterCharacter.h
@@ -191,6 +191,13 @@ private:
 	UPROPERTY()
 	class ABlasterPlayerState* BlasterPlayerState;
 
+	/*
+	* Grenade
+	*/
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* AttachedGrenade;
+
 public:	
 	void SetOverlappingWeapon(AWeapon* Weapon);
 	bool IsWeaponEqipped();
