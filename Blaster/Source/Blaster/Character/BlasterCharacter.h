79736d7f58e431e77c44e97bc4ac4f93d5eb98ea
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-06-26 22:03:54 +0100

    Frame Package
    Lecture 190 completed

--- Blaster/Source/Blaster/Character/BlasterCharacter.h
+++ Blaster/Source/Blaster/Character/BlasterCharacter.h
@@ -147,11 +147,20 @@ private:
 	UFUNCTION()
 	void OnRep_OverlappingWeapon(AWeapon* LastWeapon);
 
+	/*
+	* Components
+	*/
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	class UCombatComponent* Combat;
 
 	UPROPERTY(VisibleAnywhere)
 	class UBuffComponent* Buff;
+
+	UPROPERTY(VisibleAnywhere)
+	class ULagCompensationComponent* LagCompensation;
+
+
 
 	UFUNCTION(Server, Reliable)
 	void ServerEquipButtonPressed();
